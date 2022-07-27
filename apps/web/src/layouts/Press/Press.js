86d2bac80a5174a12c42865fcbd5631198eb6024
Jeremy Potts <jeremy@gearboxbuilt.com>
2022-07-27 14:36:51 -0400

    Adds PressHeader component

--- apps/web/src/layouts/Press/Press.js
+++ apps/web/src/layouts/Press/Press.js
@@ -4,7 +4,7 @@ import BrandAssets from '../../legacy/components/press/BrandAssets'
 import PressArticles from '../../legacy/components/press/PressArticles'
 import Layout from '../../legacy/components/Layout'
 import OurStory from '../../legacy/components/press/OurStory'
-// import PressHeader from '../../legacy/components/headers/PressHeader'
+import PressHeader from '../../legacy/components/headers/PressHeader'
 import ReadyToGrow from '../../legacy/components/product/ReadyToGrow'
 // import SEO from '../../legacy/components/SEO'
 import SpeakingInquiries from '../../legacy/components/press/SpeakingInquiries'
@@ -18,7 +18,7 @@ const Press = () => (
       title="Press & Media Kit | Leadpages"
       description="Don't want to miss anything at Leadpages? Keep updated with our announcements, press releases, and media coverage right here."
     /> */}
-    {/* <PressHeader /> */}
+    <PressHeader />
     <OurStory />
     <PressArticles />
     <ThreeColumn />
--- apps/web/src/legacy/components/headers/PressHeader.jsx
+++ apps/web/src/legacy/components/headers/PressHeader.jsx
@@ -1,124 +1,119 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import { styled } from '@design'
 // images
-import bkgSVG from '../../assets/images/shapes/wavy-lines-hourglass-gray.svg';
+import bkgSVG from '../../assets/images/shapes/wavy-lines-hourglass-gray.svg'
 
-const OuterContainer = styled.div`
-  position: relative;
-  margin-top: -60px;
-  padding-top: 60px;
-  background-color: #f7f7f7;
-  z-index: -2;
-  overflow: hidden;
-`;
+const OuterContainer = styled('div', {
+  position: 'relative',
+  mt: '-60px',
+  pt: '60px',
+  bc: '$grayAlt',
+  z: -2,
+  o: 'hidden',
+})
 
-const HeaderContainer = styled.div`
-  max-width: 1140px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 6rem;
-  padding-bottom: 6rem;
-  padding-right: 3rem;
-  padding-left: 3rem;
-  @media (min-width: 992px) {
-    padding-right: 6rem;
-    padding-left: 6rem;
-  }
-`;
+const HeaderContainer = styled('div', {
+  mw: '1140px',
+  mx: 'auto',
+  py: '6rem',
+  px: '3rem',
 
-const FlexRow = styled.div`
-  padding-top: 3rem;
-  display: flex;
-  justify-content: space-between;
-  padding-bottom: 3rem;
-  @media (max-width: 576px) {
-    display: block;
-  }
-`;
+  '@>m': {
+    px: '6rem',
+  },
+})
 
-const FlexRowItem = styled.div`
-  min-height: 1px;
-  position: relative;
-  text-align: center;
-  text-decoration: none;
-  width: 100%;
-`;
+const FlexRow = styled('div', {
+  d: 'flex',
+  jc: 'space-between',
+  py: '3rem',
 
-const FlexRowLeft = styled(FlexRowItem)`
-  justify-content: space-between;
-  text-align: left;
-  @media (min-width: 576px) {
-    -webkit-box-flex: 0;
-    -ms-flex: 0 0 100%;
-    flex: 0 0 100%;
-    max-width: 100%;
-  }
-  @media (min-width: 768px) {
-    -webkit-box-flex: 0;
-    -ms-flex: 0 0 60%;
-    flex: 0 0 60%;
-    max-width: 60%;
-  }
-  @media (min-width: 992px) {
-    margin-bottom: 0rem;
-    -webkit-box-flex: 0;
-    -ms-flex: 0 0 50%;
-    flex: 0 0 50%;
-    max-width: 50%;
-  }
-`;
+  '@>s': {
+    d: 'block',
+  },
+})
 
-const SmallHeading = styled.div`
-  font-family: 'Space Mono';
-  opacity: 0.5;
-  color: #000000;
-  font-size: 12px;
-  letter-spacing: 2px;
-  line-height: 18px;
-  text-transform: uppercase;
-`;
+const FlexRowItem = styled('div', {
+  minHeight: '1px',
+  position: 'relative',
+  ta: 'center',
+  textDecoration: 'none',
+  w: '100%',
+})
 
-const LeftHeading = styled.div`
-  margin-top: 1rem;
-  font-family: 'Value Serif';
-  font-size: 2.5rem;
-  letter-spacing: -0.03125rem;
-  line-height: 3rem;
-  color: #0f0c09;
-  margin-bottom: 2rem;
-  @media (max-width: 576px) {
-    font-size: 1.5rem;
-    line-height: 1.75rem;
-    letter-spacing: 0;
-  }
-`;
+const FlexRowLeft = styled(FlexRowItem, {
+  jc: 'space-between',
+  ta: 'left',
 
-const SVGContainer = styled.img`
-  position: absolute;
-  top: -20%;
-  right: 0;
-  overflow: hidden;
-  bottom: 0;
-  z-index: -1;
-  height: 130%;
-`;
+  '@>s': {
+    f: '0 0 100%',
+    mw: '100%',
+  },
 
-const PressHeader = () => {
-  return (
-    <OuterContainer>
-      <SVGContainer src={bkgSVG} alt="background svg" />
-      <HeaderContainer>
-        <FlexRow>
-          <FlexRowLeft>
-            <h1>
-              <SmallHeading>Leadpages Press & Media</SmallHeading>
-            </h1>
-            <LeftHeading>The latest news, updates and resources on Leadpages</LeftHeading>
-          </FlexRowLeft>
-        </FlexRow>
-      </HeaderContainer>
-    </OuterContainer>
-  );
-};
+  '@media (min-width: 768px)': {
+    f: '0 0 60%',
+    mw: '60%',
+  },
 
-export default PressHeader;
+  '@>m': {
+    mb: '0rem',
+    f: '0 0 50%',
+    mw: '50%',
+  },
+})
+
+const SmallHeading = styled('div', {
+  fontFamily: 'Space Mono',
+  opacity: 0.5,
+  c: '$black',
+  fontSize: '12px',
+  letterSpacing: '2px',
+  lineHeight: '18px',
+  tt: 'uppercase',
+})
+
+const LeftHeading = styled('div', {
+  mt: '1rem',
+  fontFamily: 'Value Serif',
+  fontSize: '2.5rem',
+  letterSpacing: '-0.03125rem',
+  lineHeight: '3rem',
+  c: '$text',
+  mb: '2rem',
+
+  '@<s': {
+    fontSize: '1.5rem',
+    lineHeight: '1.75rem',
+    letterSpacing: 0,
+  },
+})
+
+const BackgroundSVG = styled('img', {
+  position: 'absolute',
+  top: '-20%',
+  right: 0,
+  o: 'hidden',
+  bottom: 0,
+  z: '$under',
+  h: '130%',
+})
+
+const PressHeader = () => (
+  <OuterContainer>
+    <BackgroundSVG src={bkgSVG.src} alt="background svg" />
+    <HeaderContainer>
+      <FlexRow>
+        <FlexRowLeft>
+          <h1>
+            <SmallHeading>Leadpages Press & Media</SmallHeading>
+          </h1>
+          <LeftHeading>
+            The latest news, updates and resources on Leadpages
+          </LeftHeading>
+        </FlexRowLeft>
+      </FlexRow>
+    </HeaderContainer>
+  </OuterContainer>
+)
+
+export default PressHeader
