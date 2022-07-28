7c7234b1742d128254827ed7ac97f6d768a009cf
Troy McGinnis <troy@gearboxbuilt.com>
2022-07-28 11:28:23 -0700

    Ports over CustomerStoriesRotator (#82)
    * Ports over CustomerStoriesRotator

--- apps/web/src/legacy/components/rotators/CustomerStoriesRotator.jsx
+++ apps/web/src/legacy/components/rotators/CustomerStoriesRotator.jsx
@@ -1,242 +1,246 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
-import Image from 'next/image'
+import Image from '@components/Image'
 import { fadeIn } from 'react-animations'
-import styled, { keyframes } from 'styled-components'
+import { styled, keyframes } from '@design'
 // components
 import NavigationArrows from './NavigationArrows'
 import PaginationDots from './PaginationDots'
 import ReactSlick from './ReactSlick_Base'
 // images
-import WavyLineDiagnoalGraySVG from '../../assets/images/shapes/wavy-line-diagonal-gray.svg'
-import ArrowRightSVG from '../../assets/images/global/arrow_right_purple.svg'
+import WavyLineDiagonalGraySVG from '@legacy/assets/images/shapes/wavy-line-diagonal-gray.svg'
+import ArrowRightSVG from '@legacy/assets/images/global/arrow_right_purple.svg'
 
-import gregImage from '../../assets/images/customers/greg/Greg-Benz-Photography_1440px@2x.jpg'
-import jodyImage from '../../assets/images/customers/jody/Jody-Just-be-balanced_yoga@2x.jpg'
-import kaileiImage from '../../assets/images/customers/kailei/Hero-Kailei-Carr_Leadpages_full-width_1440px@2x.jpg'
-import kateImage from '../../assets/images/customers/kate/Hero-Kate-Wilkinson_Leadpages_full-width@2x.jpg'
-import sayerImage from '../../assets/images/customers/sayer/Sayer-Hero-full-width@2x.jpg'
-import shohawkImage from '../../assets/images/customers/shohawk/ShoHawk-Hero@2x.jpg'
+import gregImage from '@legacy/assets/images/customers/greg/Greg-Benz-Photography_1440px@2x.jpg'
+import jodyImage from '@legacy/assets/images/customers/jody/Jody-Just-be-balanced_yoga@2x.jpg'
+import kaileiImage from '@legacy/assets/images/customers/kailei/Hero-Kailei-Carr_Leadpages_full-width_1440px@2x.jpg'
+import kateImage from '@legacy/assets/images/customers/kate/Hero-Kate-Wilkinson_Leadpages_full-width@2x.jpg'
+import sayerImage from '@legacy/assets/images/customers/sayer/Sayer-Hero-full-width@2x.jpg'
+import shohawkImage from '@legacy/assets/images/customers/shohawk/ShoHawk-Hero@2x.jpg'
 
-const OuterContainer = styled.div`
-  position: relative;
-  background: #fff;
-  width: 100%;
-`
+const OuterContainer = styled('div', {
+  position: 'relative',
+  background: '$white',
+  width: '100%',
+})
 
-const BackgroundSVG = styled(Image)`
-  position: absolute;
-  z-index: -1;
-  top: -75%;
-  left: 20%;
-  max-width: 1140px;
-  @media (max-width: 900px) {
-    width: 100%;
-    top: -50%;
-    left: 0;
-  }
-  @media (max-width: 600px) {
-    top: -75%;
-    left: 0;
-  }
-`
+const BackgroundSVG = styled('img', {
+  position: 'absolute',
+  zIndex: -1,
+  top: '-75%',
+  left: '20%',
+  maxWidth: '1140px',
 
-const InnerContainer = styled.div`
-  max-width: 1140px;
-  margin: auto;
-`
+  '@media (max-width: 900px)': {
+    width: '100%',
+    top: '-50%',
+    left: 0,
+  },
 
-const DetailContainer = styled.div`
-  width: 100%;
-  margin-bottom: 2rem;
-  position: relative;
-`
+  '@media (max-width: 600px)': {
+    top: '-75%',
+    left: 0,
+  },
+})
 
-const FadeInAnimation = keyframes`${fadeIn}`
+const InnerContainer = styled('div', {
+  maxWidth: '1140px',
+  margin: 'auto',
+})
 
-const MainImage = styled(Image)`
-  width: 75%;
-  animation: 0.5s ${FadeInAnimation};
-  @media (max-width: 900px) {
-    width: 100%;
-    margin-bottom: 10rem;
-  }
-  @media (max-width: 425px) {
-    margin-bottom: 12rem;
-  }
-`
+const DetailContainer = styled('div', {
+  width: '100%',
+  marginBottom: '2rem',
+  position: 'relative',
+})
 
-const TextContainer = styled.div`
-  background-color: white;
-  position: absolute;
-  right: 0;
-  width: 35%;
-  padding: 4rem;
-  padding-right: 1rem;
-  margin-right: 4rem;
-  box-sizing: content-box;
-  @media (min-width: 901px) {
-    right: 0;
-    top: 15%;
-  }
-  @media (max-width: 900px) {
-    left: 0;
-    bottom: -9rem;
-    padding: 2rem;
-    margin-right: 0;
-    width: 65%;
-  }
-  @media (max-width: 425px) {
-    width: 75%;
-    bottom: -11rem;
-  }
-`
+const FadeInAnimation = keyframes(fadeIn)
 
-const Supertitle = styled.div`
-  font-family: 'Space Mono';
-  font-size: 12px;
-  letter-spacing: 2px;
-  line-height: 18px;
-  text-transform: uppercase;
-  opacity: 0.5;
-  color: #0f0c09;
-  margin-bottom: 1rem;
-`
+const MainImage = styled(Image, {
+  width: '75%',
+  animation: `0.5s ${FadeInAnimation}`,
 
-const Title = styled.div`
-  font-family: 'Value Serif';
-  font-size: 30px;
-  letter-spacing: 0.5;
-  line-height: 36px;
-  margin-bottom: 2rem;
-  color: #0f0c09;
-  @media (max-width: 1023px) {
-    font-size: 20px;
-    line-height: 24px;
-    letter-spacing: 1.12;
-    margin-bottom: 1rem;
-  }
-`
+  '@media (max-width: 900px)': {
+    width: '100%',
+    marginBottom: '10rem',
+  },
 
-const LinkArrowSVG = styled(Image)`
-  height: 12px;
-  width: 12px;
-  position: relative;
-  margin-left: 5px;
-  cursor: pointer;
-  filter: brightness(0) saturate(100%) invert(23%) sepia(48%) saturate(6317%)
-    hue-rotate(247deg) brightness(104%) contrast(105%);
-`
+  '@media (max-width: 425px)': {
+    marginBottom: '12rem',
+  },
+})
 
-const StyledLink = styled(Link)`
-  font-weight: 500;
-  text-decoration: none;
-  color: #603eff;
-  cursor: pointer;
-  &:hover {
-    color: #4d32cc;
-    ${LinkArrowSVG} {
-      filter: brightness(0) saturate(100%) invert(25%) sepia(83%)
-        saturate(2477%) hue-rotate(242deg) brightness(77%) contrast(109%);
-    }
-  }
-`
+const TextContainer = styled('div', {
+  backgroundColor: 'white',
+  position: 'absolute',
+  right: 0,
+  width: '35%',
+  padding: '4rem',
+  paddingRight: '1rem',
+  marginRight: '4rem',
+  boxSizing: 'content-box',
 
-const SlickRotator = styled(ReactSlick)`
-  margin: 0 -31px;
-  @media (max-width: 1200px) {
-    margin: 0 4rem;
-  }
-  @media (max-width: 900px) {
-    margin: 0;
-  }
+  '@media (min-width: 901px)': {
+    right: 0,
+    top: '15%',
+  },
 
-  @media (min-width: 1201px) and (max-width: 1350px) {
-    width: 90%;
-    margin: auto;
-  }
+  '@media (max-width: 900px)': {
+    left: 0,
+    bottom: '-9rem',
+    padding: '2rem',
+    marginRight: 0,
+    width: '65%',
+  },
 
-  .slick-slide > div {
-    margin: 0 1rem;
-  }
-  .slick-slide > div > div {
-    outline: none;
-  }
-`
+  '@media (max-width: 425px)': {
+    width: '75%',
+    bottom: '-11rem',
+  },
+})
 
-const ThumbnailImage = styled(Image)`
-  width: 100%;
-  height: 100%;
-`
+const Supertitle = styled('div', {
+  fontFamily: `'Space Mono'`,
+  fontSize: '12px',
+  letterSpacing: '2px',
+  lineHeight: '18px',
+  textTransform: 'uppercase',
+  opacity: 0.5,
+  color: '$text',
+  marginBottom: '1rem',
+})
 
-const ThumbnailOverlayBG = styled.div`
-  display: none;
-  position: absolute;
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(96, 62, 255, 0.9);
-`
+const Title = styled('div', {
+  fontFamily: `'Value Serif'`,
+  fontSize: '30px',
+  letterSpacing: 0.5,
+  lineHeight: '36px',
+  marginBottom: '2rem',
+  color: '$text',
 
-const ThumbnailSection = styled.div`
-  position: relative;
-  cursor: pointer;
-`
+  '@media (max-width: 1023px)': {
+    fontSize: '20px',
+    lineHeight: '24px',
+    letterSpacing: 1.12,
+    marginBottom: '1rem',
+  },
+})
 
-const ThumbnailOverlayText = styled.div`
-  margin: 0;
-  position: absolute;
-  width: 75%;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: #fff;
-  text-align: center;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 500;
-  font-family: 'Apercu Pro';
-  max-width: 250px;
-  @media (min-width: 1201px) {
-    font-size: 18px;
-    line-height: 30px;
-  }
-`
+const LinkArrowSVG = styled('img', {
+  height: '12px',
+  width: '12px',
+  position: 'relative',
+  marginLeft: '5px',
+  cursor: 'pointer',
+  filter: `brightness(0) saturate(100%) invert(23%) sepia(48%) saturate(6317%)
+    hue-rotate(247deg) brightness(104%) contrast(105%)`,
+})
 
-const ImageContainer = styled.div`
-  position: relative;
-  cursor: pointer;
-  width: 100%;
-  height: 100%;
-  &:hover ${ThumbnailOverlayBG} {
-    display: block;
-  }
-  &.active-slide ${ThumbnailOverlayBG} {
-    display: block;
-  }
-`
+const StyledLink = styled('a', {
+  fontWeight: 500,
+  textDecoration: 'none',
+  color: '$primary',
+  cursor: 'pointer',
 
-const RotatorContainer = styled.div``
+  '&:hover': {
+    color: '$indigoDark',
+    [`${LinkArrowSVG}`]: {
+      filter: `brightness(0) saturate(100%) invert(25%) sepia(83%)
+        saturate(2477%) hue-rotate(242deg) brightness(77%) contrast(109%)`,
+    },
+  },
+})
+
+const SlickRotator = styled(ReactSlick, {
+  margin: '0 -31px',
+
+  '@media (max-width: 1200px)': {
+    margin: '0 4rem',
+  },
+
+  '@media (max-width: 900px)': {
+    margin: 0,
+  },
+
+  '@media (min-width: 1201px) and (max-width: 1350px)': {
+    width: '90%',
+    margin: 'auto',
+  },
+
+  '.slick-slide > div': {
+    margin: '0 1rem',
+  },
+
+  '.slick-slide > div > div': {
+    outline: 'none',
+  },
+})
+
+const ThumbnailImage = styled(Image, {
+  width: '100%',
+  height: '100%',
+})
+
+const ThumbnailOverlayBG = styled('div', {
+  display: 'none',
+  position: 'absolute',
+  zIndex: 1,
+  width: '100%',
+  height: '100%',
+  backgroundColor: 'rgba(96, 62, 255, 0.9)',
+})
+
+const ThumbnailSection = styled('div', {
+  position: 'relative',
+  cursor: 'pointer',
+})
+
+const ThumbnailOverlayText = styled('div', {
+  margin: 0,
+  position: 'absolute',
+  width: '75%',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  color: '$white',
+  textAlign: 'center',
+  fontSize: '16px',
+  lineHeight: '24px',
+  fontWeight: 500,
+  fontFamily: `'Apercu Pro'`,
+  maxWidth: '250px',
+
+  '@media (min-width: 1201px)': {
+    fontSize: '18px',
+    lineHeight: '30px',
+  },
+})
+
+const ImageContainer = styled('div', {
+  position: 'relative',
+  cursor: 'pointer',
+  width: '100%',
+  height: '100%',
+
+  [`&:hover ${ThumbnailOverlayBG}`]: {
+    display: 'block',
+  },
+
+  [`&.active-slide ${ThumbnailOverlayBG}`]: {
+    display: 'block',
+  },
+})
+
+const RotatorContainer = styled('div', {})
 
 const CustomerStoriesRotator = ({ showBackgroundImage }) => {
   const [activeIndex, setActiveIndex] = useState(0)
 
-  const images = {
-    gregImage,
-    jodyImage,
-    kaileiImage,
-    kateImage,
-    sayerImage,
-    shohawkImage,
-  }
-
-  const getImage = (image) => image
-
   const customerFeaturettesArray = [
     {
       name: 'Kailei',
-      image: getImage(images.kaileiImage),
+      image: kaileiImage,
       quote: {
         title:
           'Kailei uses Leadpages to book her clients and sell her consulting services.',
@@ -250,7 +254,7 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
     },
     {
       name: 'Kate',
-      image: getImage(images.kateImage),
+      image: kateImage,
       quote: {
         title:
           'Kate uses Leadpages to pinpoint her audience and sell her marketing services.',
@@ -264,7 +268,7 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
     },
     {
       name: 'Greg',
-      image: getImage(images.gregImage),
+      image: gregImage,
       quote: {
         title:
           'Greg uses Leadpages to sell his photography software and educate his audience.',
@@ -278,7 +282,7 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
     },
     {
       name: 'Shohawk',
-      image: getImage(images.shohawkImage),
+      image: shohawkImage,
       quote: {
         title:
           'Michael and Chris use Leadpages to promote and sell their videos.',
@@ -292,7 +296,7 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
     },
     {
       name: 'Jody',
-      image: getImage(images.jodyImage),
+      image: jodyImage,
       quote: {
         title:
           'Jody uses Leadpages to sign-up stressed professionals for her yoga sessions.',
@@ -306,7 +310,7 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
     },
     {
       name: 'Sayer',
-      image: getImage(images.sayerImage),
+      image: sayerImage,
       quote: {
         title:
           'Sayer Payne uses Leadpages to connect customers with retailers.',
@@ -319,12 +323,14 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
       overlayText: 'Ecommerce',
     },
   ]
+
   const rotatorSwiped = () => {
     window.dataLayer = window.dataLayer || []
     window.dataLayer.push({
       event: 'customerRotatorSwiped',
     })
   }
+
   const settings = {
     arrows: true,
     centerMode: true,
@@ -395,46 +401,55 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
       setActiveIndex(index)
     },
   }
-  console.log(customerFeaturettesArray[activeIndex].image)
+
   return (
     <OuterContainer>
       {showBackgroundImage && (
         <BackgroundSVG
-          src={WavyLineDiagnoalGraySVG}
+          src={WavyLineDiagonalGraySVG.svg}
           alt="background wavy line"
         />
       )}
       <InnerContainer>
         <DetailContainer>
-          <StyledLink
+          <Link
             href={customerFeaturettesArray[activeIndex].link.route}
-            alt={customerFeaturettesArray[activeIndex].quote.title}
-            data-gtm="customer-story-link"
-            data-value={customerFeaturettesArray[activeIndex].name}
+            passHref
           >
-            <MainImage
-              src={customerFeaturettesArray[activeIndex].image}
-              alt={customerFeaturettesArray[activeIndex].quote.title}
-            />
-          </StyledLink>
-          <TextContainer>
             <StyledLink
-              href={customerFeaturettesArray[activeIndex].link.route}
-              alt={customerFeaturettesArray[activeIndex].quote.title}
+              aria-label={customerFeaturettesArray[activeIndex].quote.title}
               data-gtm="customer-story-link"
               data-value={customerFeaturettesArray[activeIndex].name}
             >
-              <>
-                <Supertitle>
-                  {customerFeaturettesArray[activeIndex].quote.supertitle}
-                </Supertitle>
-                <Title>
-                  {customerFeaturettesArray[activeIndex].quote.title}
-                </Title>
-                <span>{`${customerFeaturettesArray[activeIndex].link.text}  `}</span>
-                <LinkArrowSVG src={ArrowRightSVG} alt="arrow icon" />
-              </>
+              <MainImage
+                key={customerFeaturettesArray[activeIndex].quote.title}
+                image={customerFeaturettesArray[activeIndex].image}
+                alt={customerFeaturettesArray[activeIndex].quote.title}
+              />
             </StyledLink>
+          </Link>
+          <TextContainer>
+            <Link
+              href={customerFeaturettesArray[activeIndex].link.route}
+              passHref
+            >
+              <StyledLink
+                aria-label={customerFeaturettesArray[activeIndex].quote.title}
+                data-gtm="customer-story-link"
+                data-value={customerFeaturettesArray[activeIndex].name}
+              >
+                <>
+                  <Supertitle>
+                    {customerFeaturettesArray[activeIndex].quote.supertitle}
+                  </Supertitle>
+                  <Title>
+                    {customerFeaturettesArray[activeIndex].quote.title}
+                  </Title>
+                  <span>{`${customerFeaturettesArray[activeIndex].link.text}  `}</span>
+                  <LinkArrowSVG src={ArrowRightSVG.src} alt="arrow icon" />
+                </>
+              </StyledLink>
+            </Link>
           </TextContainer>
         </DetailContainer>
         <RotatorContainer>
@@ -460,7 +475,7 @@ const CustomerStoriesRotator = ({ showBackgroundImage }) => {
                         </ThumbnailOverlayText>
                       </ThumbnailOverlayBG>
                       <div id="slide-image">
-                        <ThumbnailImage src={image} alt={quote.title} />
+                        <ThumbnailImage image={image} alt={quote.title} />
                       </div>
                     </ImageContainer>
                   </ThumbnailSection>
