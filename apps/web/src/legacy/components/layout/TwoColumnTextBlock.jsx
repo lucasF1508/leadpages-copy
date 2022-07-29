a8695ac3c20a2f0da9684afa097a3a7e254c0836
Troy McGinnis <troy@gearboxbuilt.com>
2022-07-28 17:11:01 -0700

    TwoColumnTextBlock (#89)
    Adds TwoColumnTextBlock

--- apps/web/src/legacy/components/layout/TwoColumnTextBlock.jsx
+++ apps/web/src/legacy/components/layout/TwoColumnTextBlock.jsx
@@ -1,99 +1,108 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'next/link'
-import styled from 'styled-components'
-// images
+import { styled } from '@design'
+
+// Images
 import arrowRightPurpleSVG from '../../assets/images/global/arrow_right_purple.svg'
 
-const OuterContainer = styled.div`
-  position: relative;
-`
+const OuterContainer = styled('div', {
+  position: 'relative',
+})
 
-const InnerContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  max-width: 1140px;
-  margin: 0 auto 3rem;
-  padding: 0 3rem;
-  @media (max-width: 600px) {
-    flex-direction: column;
-  }
-`
+const InnerContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  maxWidth: '1140px',
+  margin: '0 auto 3rem',
+  padding: '0 3rem',
 
-const TextBlock = styled.div`
-  padding: 0 1rem;
-  width: 100%;
-  max-width: 300px;
-  @media (max-width: 900px) {
-    max-width: 250px;
-  }
-  @media (max-width: 600px) {
-    padding: 1rem 0;
-    max-width: 100%;
-  }
-`
+  '@media (max-width: 600px)': {
+    flexDirection: 'column',
+  },
+})
 
-const Heading = styled.div`
-  font-size: 16px;
-  font-weight: 500;
-  line-height: 24px;
-  margin-bottom: 1rem;
-  color: #0f0c09;
-`
+const TextBlock = styled('div', {
+  padding: '0 1rem',
+  width: '100%',
+  maxWidth: '300px',
 
-const MainText = styled.div`
-  font-size: 14px;
-  line-height: 20px;
-  margin-bottom: 1rem;
-  color: #575452;
-`
+  '@media (max-width: 900px)': {
+    maxWidth: '250px',
+  },
 
-const LinkText = styled.div`
-  color: #603eff;
-  font-size: 16px;
-  line-height: 30px;
-  text-align: left;
-  font-weight: 500;
-`
+  '@media (max-width: 600px)': {
+    padding: '1rem 0',
+    maxWidth: '100%',
+  },
+})
 
-const ArrowSVG = styled.img`
-  width: 20px;
-  height: 10px;
-  position: absolute;
-  top: 10px;
-  margin-left: 4px;
-`
+const Heading = styled('div', {
+  fontSize: '16px',
+  fontWeight: 500,
+  lineHeight: '24px',
+  marginBottom: '1rem',
+  color: '$text',
+})
 
-const StyledLink = styled.a`
-  position: relative;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  cursor: pointer;
-  &:hover ${LinkText} {
-    color: #4d32cc;
-  }
-  &:hover ${ArrowSVG} {
-    filter: invert(18%) sepia(97%) saturate(3719%) hue-rotate(249deg)
-      brightness(81%) contrast(95%);
-  }
-`
+const MainText = styled('div', {
+  fontSize: '14px',
+  lineHeight: '20px',
+  marginBottom: '1rem',
+  color: '$textAlt',
+})
 
-const GatsbyLink = styled(Link)`
-  position: relative;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  cursor: pointer;
-  &:hover ${LinkText} {
-    color: #4d32cc;
-  }
-  &:hover ${ArrowSVG} {
-    filter: invert(18%) sepia(97%) saturate(3719%) hue-rotate(249deg)
-      brightness(81%) contrast(95%);
-  }
-`
+const LinkText = styled('div', {
+  color: '$primary',
+  fontSize: '16px',
+  lineHeight: '30px',
+  textAlign: 'left',
+  fontWeight: 500,
+  position: 'relative',
+})
+
+const ArrowSVG = styled('img', {
+  width: '20px',
+  height: '10px',
+  position: 'absolute',
+  top: '10px',
+  marginLeft: '4px',
+})
+
+const StyledLink = styled('a', {
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  textDecoration: 'none',
+  cursor: 'pointer',
+
+  [`&:hover ${LinkText}`]: {
+    color: '$primary',
+  },
+
+  [`&:hover ${ArrowSVG}`]: {
+    filter: `invert(18%) sepia(97%) saturate(3719%) hue-rotate(249deg)
+      brightness(81%) contrast(95%)`,
+  },
+})
+
+const NextLink = styled('a', {
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  textDecoration: 'none',
+  cursor: 'pointer',
+
+  [`&:hover ${LinkText}`]: {
+    color: '$indigoDark',
+  },
+
+  [`&:hover ${ArrowSVG}`]: {
+    filter: `invert(18%) sepia(97%) saturate(3719%) hue-rotate(249deg)
+      brightness(81%) contrast(95%)`,
+  },
+})
 
 const TwoColumnTextBlock = ({ textBlockArray }) => {
   return (
@@ -116,22 +125,24 @@ const TwoColumnTextBlock = ({ textBlockArray }) => {
                   <LinkText>
                     {linkText}
                     <ArrowSVG
-                      src={arrowRightPurpleSVG}
+                      src={arrowRightPurpleSVG.src}
                       alt="purple right arrow"
                     />
                   </LinkText>
                 </StyledLink>
               )}
               {linkType === 'internal' && (
-                <GatsbyLink href={linkRoute} alt={linkAltText} target="_blank">
-                  <LinkText>
-                    {linkText}
-                    <ArrowSVG
-                      src={arrowRightPurpleSVG}
-                      alt="purple right arrow"
-                    />
-                  </LinkText>
-                </GatsbyLink>
+                <Link href={linkRoute} passHref>
+                  <NextLink aria-label={linkAltText} target="_blank">
+                    <LinkText>
+                      {linkText}
+                      <ArrowSVG
+                        src={arrowRightPurpleSVG.src}
+                        alt="purple right arrow"
+                      />
+                    </LinkText>
+                  </NextLink>
+                </Link>
               )}
             </TextBlock>
           )
