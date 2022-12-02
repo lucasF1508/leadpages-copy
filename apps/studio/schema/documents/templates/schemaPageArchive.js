72409503384e16dfecc58ce57e288f0d9bca236a
Troy McGinnis <troy@gearboxbuilt.com>
2022-12-02 10:00:17 -0800

    Adds Comparisons schema, some clean up

--- apps/studio/schema/documents/templates/schemaPageArchive.js
+++ apps/studio/schema/documents/templates/schemaPageArchive.js
@@ -2,13 +2,14 @@ import { BsArchive as icon } from 'react-icons/bs'
 import startCase from 'lodash/startCase'
 import { F, FS, G, P } from 'part:gearbox-schema-tool/schema-builder'
 
-const pageTemplates = ['post', 'customer', 'integration']
+const pageTemplates = ['post', 'customer', 'integration', 'comparison']
 
 export const schemaPageArchive = {
   icon,
   name: 'pageArchive',
   title: 'Archive Page',
   type: 'document',
+  __experimental_actions: ['update', 'publish' /* 'create', 'delete' */],
   groups: [
     ...G.fieldGroupDefaults(),
     G.fieldGroup('seo', { title: 'SEO' }),
@@ -56,7 +57,8 @@ export const schemaPageArchive = {
         title: 'Redirect to legacy page',
         description:
           'Enable to redirect to a legacy Leadpages page, if it exists.',
-        hidden: ({ parent }) => parent?.slug?.current !== 'integrations',
+        hidden: ({ parent }) =>
+          !['integrations', 'comparisons'].includes(parent?.slug?.current),
       }),
       F.object({
         name: 'options',
