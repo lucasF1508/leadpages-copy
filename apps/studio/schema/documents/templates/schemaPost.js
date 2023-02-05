56f96df7d9353fa6fa25e4b23280a0304f88e28a
Troy McGinnis <troy@gearboxbuilt.com>
2023-02-04 21:46:51 -0800

    Blog updates

--- apps/studio/schema/documents/templates/schemaPost.js
+++ apps/studio/schema/documents/templates/schemaPost.js
@@ -6,10 +6,18 @@ export const schemaPost = {
   name: 'post',
   title: 'Posts',
   type: 'document',
+  orderings: [
+    {
+      title: 'Published',
+      name: 'publishedDesc',
+      by: [{ field: 'publishedDate', direction: 'desc' }],
+    },
+  ],
   groups: [
     ...G.fieldGroupDefaults(),
     G.fieldGroup('excerpt', { title: 'Excerpt' }),
     G.fieldGroup('seo', { title: 'SEO' }),
+    G.fieldGroup('options', { title: 'Page Options' }),
   ],
   fieldsets: [FS.seo(), FS.fieldset('meta', { collapsed: false })],
   fields: [
@@ -26,6 +34,15 @@ export const schemaPost = {
       F.multiReference('categoryPost', { name: 'secondaryCategories' }),
     ]),
     ...G.group('seo', [F.seo()]),
+    ...G.group('options', [
+      F.boolean({
+        name: 'redirectToLegacy',
+        title: 'Proxy to legacy post',
+        description:
+          'Enable to proxy to the legacy Leadpages post, if it exists.',
+        initialValue: false,
+      }),
+    ]),
   ],
   preview: P.titleImage(),
 }
