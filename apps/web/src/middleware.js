3fab8ebe022accd9dffc5cd505a1dad3bf655091
Troy McGinnis <troy@gearboxbuilt.com>
2023-02-06 01:56:29 -0800

    Reverts proxy troubleshooting

--- apps/web/src/middleware.js
+++ apps/web/src/middleware.js
@@ -1,9 +1,6 @@
 import { NextResponse } from 'next/server'
 import incrementalPaths from '@public/indices/incrementalPaths'
 
-// const proxyHost = process.env.LEADPAGES_BLOG_PROXY_HOST
-// const proxyPath = process.env.LEADPAGES_BLOG_PROXY_PATH
-
 const patterns = incrementalPaths?.map(
   (pathname) => new URLPattern({ pathname })
 )
@@ -11,10 +8,6 @@ const patterns = incrementalPaths?.map(
 export async function middleware(request) {
   const response = NextResponse.next()
   const url = request.nextUrl.clone()
-
-  // if (url.pathname.startsWith(proxyPath)) {
-  //   return NextResponse.rewrite(new URL(`${url.pathname}/`, proxyHost))
-  // }
 
   if (!incrementalPaths.length) return response
 
