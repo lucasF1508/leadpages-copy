4fceda0963fd9bb1d7ecf1d00e06110010c2c742
Troy McGinnis <troy@gearboxbuilt.com>
2023-02-06 00:59:28 -0800

    Alert bar updates

--- apps/web/src/legacy/data/alert-bar_data.jsx
+++ apps/web/src/legacy/data/alert-bar_data.jsx
@@ -2,51 +2,43 @@
 /* when read, alert bars are rendered in order of specificity  */
 /* i.e. lower index takes precedence over higher index */
 
+const commonData = {
+  src: 'https://embed.lpcontent.net/leadbars/current/embed.js',
+  domain: 'lps.lpages.co',
+}
+
 // eslint-disable-next-line import/prefer-default-export
 export const alertBarData = [
   {
     name: 'Alert bar product page',
     data: {
-      src: 'https://static.leadpages.com/leadbars/current/embed.js',
       id: 'BdpWkphuCXVs8t2vbrYeLj',
-      domain: 'lps.leadpages.net',
+      ...commonData,
     },
     placementRegex: 'product/alert-bars',
   },
   {
     name: 'Landing page template gallery',
     data: {
-      src: 'https://embed.lpcontent.net/leadbars/current/embed.js',
       id: 'Pp42Z5mhbYq6MXMpeJ3sce',
-      domain: 'lps.lpages.co',
+      ...commonData,
     },
     placementRegex: 'templates',
   },
   {
     name: 'Silo pages',
     data: {
-      src: 'https://static.leadpages.com/leadbars/current/embed.js',
       id: '2Rmmmc8LVAYsUVdHBkRAgS',
-      domain: 'lps.lpages.co',
+      ...commonData,
     },
     placementRegex:
-      'landing-pages-guide/|lead-generation-guide/|conversion-optimization-guide/',
+      'landing-pages-guide|lead-generation-guide|conversion-optimization-guide',
   },
   // {
   //   name: 'Global',
   //   data: {
-  //     src: 'https://static.leadpages.com/leadbars/current/embed.js',
   //     id: 'LJJyfxPJ6e6fDdXPSLw8N7',
-  //     domain: 'lps.lpages.co',
-  //   },
-  //   placementRegex: '.*',
-  // },
-  // {
-  //   name: 'Global',
-  //   data: {
-  //     src: '',
-  //     id: '',
-  //     domain: '',
+  //     ...commonData,
   //   },
   //   placementRegex: '.*',
   // },
