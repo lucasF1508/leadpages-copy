fb2ffc99a277d68c1552a367a490d49e72259e4d
Troy McGinnis <troy@gearboxbuilt.com>
2023-04-11 16:36:27 -0700

    Updates sentry denyUrls

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -6,5 +6,5 @@ Sentry.init({
   dsn: SENTRY_DSN,
   environment: process.env.NODE_ENV,
   enabled: process.env.NODE_ENV === 'production',
-  denyUrls: [/https?:\/\/((cdn|www)\.)?clickcease\.com/],
+  denyUrls: [/https?:\/\/((monitor|www)\.)?clickcease\.com/],
 })
