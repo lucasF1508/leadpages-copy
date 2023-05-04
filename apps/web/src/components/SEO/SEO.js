74eb954ff58584938986badd39427d5c37d218e9
Troy McGinnis <troy@gearboxbuilt.com>
2023-05-04 15:37:42 -0700

    v1.14.0

--- apps/web/src/components/SEO/SEO.js
+++ apps/web/src/components/SEO/SEO.js
@@ -94,7 +94,10 @@ const SEO = (props) => {
           name="viewport"
           content="width=device-width, initial-scale=1.0, viewport-fit=cover"
         />
-        <meta name="p:domain_verify" content="11470417057f03716e17affcb71896ef"/>
+        <meta
+          name="p:domain_verify"
+          content="11470417057f03716e17affcb71896ef"
+        />
         {image?.url && <meta name="image" content={image?.url} />}
         {url && <link rel="canonical" href={url} />}
         {url && <meta property="og:url" content={url} />}
