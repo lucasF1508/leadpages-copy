5350b1a1cb25ca90d4b89605a54eda52309ac1fc
Noah Jeffrey <noah.jeffrey@leadpages.com>
2023-11-10 12:26:49 -0600

    LPE-3494 Fixed Template Sitemaps (#272)
    * LPE-3494 Fixed Template Sitemaps
    
    We can now fetch all page and site templates for our template sitemaps.
    
    * LPE-3494 Added Base Filters
    
    This will ensure we add the same templates to the sitemap that we show in the template gallery!

--- apps/web/src/pages/sitemap/templates/leadpages-templates.js
+++ apps/web/src/pages/sitemap/templates/leadpages-templates.js
@@ -1,17 +1,29 @@
 import { getServerSideSitemapLegacy } from 'next-sitemap'
 import MandrelApi from '@lp/template-gallery/dist/mandrel-api'
-import { templatesBaseUrl } from '@legacy/constants/templates'
+import { templatesBaseUrl, baseFilters } from '@legacy/constants/templates'
 
-const mandrelApi = new MandrelApi({ baseUrl: templatesBaseUrl })
+const mandrelApi = new MandrelApi({ baseUrl: templatesBaseUrl, baseFilters })
 
 const { NEXT_PUBLIC_URL } = process.env
 
 export const getServerSideProps = async (context) => {
-  const { _items: siteTemplates = [] } = await mandrelApi.getLeadpageTemplates()
+  const limit = 300; // Max limit is 300
+  let cursor = "0";
+  let total = 0;
+  let fetchedTemplates = [];
+  do {
+    const response = await mandrelApi.getLeadpageTemplates({ 
+      limit: { operator: "", value: limit },
+      cursor: { operator: "", value: cursor }
+    });
+    cursor = response._meta.cursor;
+    total = response._meta.total;
+    fetchedTemplates = [...fetchedTemplates, ...response._items];
+  } while (fetchedTemplates.length < total);
 
   return getServerSideSitemapLegacy(
     context,
-    siteTemplates.map((item) => ({
+    fetchedTemplates.map((item) => ({
       loc: `${NEXT_PUBLIC_URL}/templates/preview/${item?._meta?.id}`,
       changefreq: 'yearly',
       priority: 0.7,
