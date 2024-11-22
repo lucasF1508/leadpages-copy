72466a47b02e22e545af7098b0fabe510dd163a5
Nick Kennedy <10602107+nkndy@users.noreply.github.com>
2024-11-22 10:29:03 -0800

    Remove Preview Sitemaps (#356)

--- apps/web/src/pages/sitemap/templates/index.xml.js
+++ apps/web/src/pages/sitemap/templates/index.xml.js
@@ -4,8 +4,6 @@ const { NEXT_PUBLIC_URL } = process.env
 
 export const getServerSideProps = async (context) =>
   getServerSideSitemapIndexLegacy(context, [
-    `${NEXT_PUBLIC_URL}/sitemap/templates/leadpages-template-preview`,
-    `${NEXT_PUBLIC_URL}/sitemap/templates/website-template-preview`,
     `${NEXT_PUBLIC_URL}/sitemap/templates/leadpages-template`,
     `${NEXT_PUBLIC_URL}/sitemap/templates/website-template`,
     `${NEXT_PUBLIC_URL}/sitemap/templates/categories`,
